9f98cd3aad9b7bc5bffb39f41080c54e8dd7df50
Uma33298039 <umamageshwari098@gmail.com>
2024-07-20 12:29:00 +0530

    Create color_codes.h

---
+++ color_codes.h
@@ -0,0 +1,7 @@
+#ifndef COLOR_CODES_H
+#define COLOR_CODES_H
+void initialize_color_codes();
+void translate_color_pair(int pair_number, char *major_color,char *minor_color);
+void print_color_manual();
+
+#endif
