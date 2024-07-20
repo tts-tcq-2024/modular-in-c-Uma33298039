c61eab4efef5f49065fb9b8557e415d4a8b20735
Uma33298039 <umamageshwari098@gmail.com>
2024-07-20 12:59:25 +0530

    Update color_codes.h

--- color_codes.h
+++ color_codes.h
@@ -1,8 +1,8 @@
 #ifndef COLOR_CODES_H_
 #define COLOR_CODES_H_
 
-enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
-enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
+typedef enum {WHITE, RED, BLACK, YELLOW, VIOLET} MajorColor;
+typedef enum {BLUE, ORANGE, GREEN, BROWN, SLATE} MinorColor;
 
 extern const char* MajorColorNames[];
 extern const char* MinorColorNames[];
@@ -16,7 +16,7 @@ typedef struct {
     enum MinorColor minorColor;
 } ColorPair;
 
-void ColorPairToString(const ColorPair* colorPair, char* buffer);
+void ColorPairToString(const ColorPair* colorPair, char* colorpairnames);
 ColorPair GetColorFromPairNumber(int pairNumber);
 int GetPairNumberFromColor(const ColorPair* colorPair);
 void PrintColorCodeManual(void);
