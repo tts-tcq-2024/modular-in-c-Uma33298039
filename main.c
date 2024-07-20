439a905943d5971faaa9157746b3eed54d3b5837
Uma33298039 <umamageshwari098@gmail.com>
2024-07-20 12:41:50 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <assert.h>
-#include "color_code_converter.h"
+#include "color_codes.h"
 
 void testNumberToPair(int pairNumber, enum MajorColor expectedMajor, enum MinorColor expectedMinor) {
     ColorPair colorPair = GetColorFromPairNumber(pairNumber);
