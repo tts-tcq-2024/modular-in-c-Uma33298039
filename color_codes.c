6c29014f4b0b50e1812a0dd1aa7888820251f71b
Uma33298039 <umamageshwari098@gmail.com>
2024-07-22 10:40:17 +0530

    Update color_codes.c

--- color_codes.c
+++ color_codes.c
@@ -3,5 +3,6 @@
 const char* MajorColorNames[] = {"White", "Red", "Black", "Yellow", "Violet"}; 
 const char* MinorColorNames[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
 
+
 int numberOfMajorColors = sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
 int numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
