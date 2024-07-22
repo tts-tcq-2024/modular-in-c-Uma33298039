d6baaef636c936e24054fbf4631bf01236bf46e0
Uma33298039 <umamageshwari098@gmail.com>
2024-07-22 10:39:33 +0530

    Update Testing.c

--- Testing.c
+++ Testing.c
@@ -8,6 +8,5 @@ int main() {
     testNumberToPair(5, WHITE, SLATE);
     testPairToNumber(BLACK, ORANGE, 12);
     testPairToNumber(VIOLET, SLATE, 25);
-    print_reference_manual();
     return 0;
 }
