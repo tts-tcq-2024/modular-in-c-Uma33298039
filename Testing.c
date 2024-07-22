58eefafb24cc936a42cc4539e2d76116ce8cadda
Uma33298039 <umamageshwari098@gmail.com>
2024-07-22 10:07:06 +0530

    Create Testing.c

---
+++ Testing.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "color_codes.h"
+#include "reference_manual_printer.h"
+#include "number_to_pair_tests.h"
+#include "pair_to_number_tests.h"
+
+int main() { 
+    testNumberToPair(4, WHITE, BROWN);
+    testNumberToPair(5, WHITE, SLATE);
+    testPairToNumber(BLACK, ORANGE, 12);
+    testPairToNumber(VIOLET, SLATE, 25);
+    return 0;
+}
