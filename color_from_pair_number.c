3697180b7fd3c84725d0af18e997f5a96071f3ca
Uma33298039 <umamageshwari098@gmail.com>
2024-07-22 10:41:47 +0530

    Update color_from_pair_number.c

--- color_from_pair_number.c
+++ color_from_pair_number.c
@@ -1,7 +1,8 @@
 #include "color_codes.h"
 #include "color_from_pair_number.h"
 
-ColorPair GetColorFromPairNumber(int pairNumber) {
+ColorPair GetColorFromPairNumber(int pairNumber)
+{
     ColorPair colorPair;
     int zeroBasedPairNumber = pairNumber - 1; 
     colorPair.majorColor = (MajorColor)(zeroBasedPairNumber / numberOfMinorColors);
