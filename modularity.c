e30082200e68c8af55175e22c1afdbc038ed16d7
Uma33298039 <umamageshwari098@gmail.com>
2024-07-22 10:38:00 +0530

    Update and rename Testing.c to modularity.c

--- modularity.c
+++ modularity.c
@@ -8,5 +8,6 @@ int main() {
     testNumberToPair(5, WHITE, SLATE);
     testPairToNumber(BLACK, ORANGE, 12);
     testPairToNumber(VIOLET, SLATE, 25);
+    print_reference_manual();
     return 0;
 }
